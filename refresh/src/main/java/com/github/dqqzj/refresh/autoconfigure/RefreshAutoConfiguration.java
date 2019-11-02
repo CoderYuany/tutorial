3ea39b85f4e2c098e22a3389291e9b0d256c82ed
dqqzj <798078824@qq.com>
2019-11-02 12:54:45 +0800

    refresh

--- refresh/src/main/java/com/github/dqqzj/refresh/autoconfigure/RefreshAutoConfiguration.java
+++ refresh/src/main/java/com/github/dqqzj/refresh/autoconfigure/RefreshAutoConfiguration.java
@@ -5,36 +5,14 @@
 
 package com.github.dqqzj.refresh.autoconfigure;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import javax.annotation.PostConstruct;
-
 import com.github.dqqzj.refresh.ContextRefresher;
 import com.github.dqqzj.refresh.event.RefreshEventListener;
 import com.github.dqqzj.refresh.scope.RefreshScope;
-import org.springframework.aop.scope.ScopedProxyUtils;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinitionHolder;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.bind.Bindable;
-import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.weaving.LoadTimeWeaverAware;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.StandardEnvironment;
-import org.springframework.instrument.classloading.LoadTimeWeaver;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @ConditionalOnClass({RefreshScope.class})
