753d870a597ffde5806bbe7162106892b1a0a117
wb-qzj584329 <wb-qzj584329@alibaba-inc.com>
2019-12-19 19:42:41 +0800

    unify

--- athena/src/main/java/com/github/dqqzj/athena/test/TestController.java
+++ athena/src/main/java/com/github/dqqzj/athena/test/TestController.java
@@ -20,6 +20,7 @@ public class TestController {
     @Autowired
     TestService testService;
     @GetMapping("hello")
+    @LogAdvice
     public ResultVO hello(Integer x,double y) {
          testService.hello(x,y);
          return ResultVO.ofSuccess("ss");
