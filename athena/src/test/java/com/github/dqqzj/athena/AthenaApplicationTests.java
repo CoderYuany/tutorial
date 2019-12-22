aa16083cb526c532aea93bf782b38d7f71e21981
dqqzj <798078824@qq.com>
2019-12-22 22:09:39 +0800

    考虑支持class级别和扫描包级别等其他手段，以做到更加灵活配置

--- athena/src/test/java/com/github/dqqzj/athena/AthenaApplicationTests.java
+++ athena/src/test/java/com/github/dqqzj/athena/AthenaApplicationTests.java
@@ -2,6 +2,7 @@ package com.github.dqqzj.athena;
 
 import com.alibaba.fastjson.JSON;
 import com.github.dqqzj.athena.agent.AgentArgs;
+import com.github.dqqzj.athena.transfer.MethodDesc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,9 +16,13 @@ class AthenaApplicationTests {
 	void contextLoads() {
 		AgentArgs args = new AgentArgs();
 		args.setLogForParams(true);
-		List<String> list = new ArrayList<>(2);
-		list.add("com.github.dqqzj.athena.test.TestController#hello(java.lang.Integer,double)");
-		list.add("com.github.dqqzj.athena.test.TestService#hello(java.lang.Integer,double)");
+		List<MethodDesc> list = new ArrayList<>(2);
+		String[] methodArgs = new String[] {"java.lang.Integer","double"};
+		MethodDesc methodDesc = new MethodDesc("com.github.dqqzj.athena.test.TestController","hello",methodArgs);
+		list.add(methodDesc);
+		String[] methodArgs1 = new String[] {"java.lang.Integer","double"};
+		MethodDesc methodDesc1 = new MethodDesc("com.github.dqqzj.athena.test.TestService","hello",methodArgs1);
+		list.add(methodDesc1);
 		args.setMethods(list);
 		System.out.println(JSON.toJSONString(args));
 	}
