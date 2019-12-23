f1c25c348d1d066c8cc5e6ff1812c7575f5d23ad
dqqzj <798078824@qq.com>
2019-12-23 23:17:16 +0800

    进行类级别的方法描述抽离

--- athena/src/main/java/com/github/dqqzj/athena/utils/AgentUtils.java
+++ athena/src/main/java/com/github/dqqzj/athena/utils/AgentUtils.java
@@ -1,6 +1,5 @@
 package com.github.dqqzj.athena.utils;
 
-import com.alibaba.fastjson.JSON;
 import com.github.dqqzj.athena.transfer.MethodDesc;
 import javassist.*;
 import lombok.extern.slf4j.Slf4j;
